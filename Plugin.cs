3b484299cc5825481c73be2c318c00495f9fc94c
ZlothY29IQ <211057194+ZlothY29IQ@users.noreply.github.com>
2025-10-11 13:01:31 +0100

    update version

--- Plugin.cs
+++ Plugin.cs
@@ -19,7 +19,7 @@ using static WalkSimulator.Tools.AssetUtils;
 
 namespace WalkSimulator
 {
-    [BepInPlugin("kylethescientist.zlothy.walksimulator", "WalkSimulator", "1.0.2")]
+    [BepInPlugin("kylethescientist.zlothy.walksimulator", "WalkSimulator", "1.0.3")]
     public class Plugin : BaseUnityPlugin
     {
         public static Plugin Instance { get; private set; }
